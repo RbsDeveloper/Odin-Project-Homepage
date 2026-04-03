0c8d8676f4104e817ed31e459259192d7041595e
RbsDeveloper <developer.rbs@gmail.com>
2026-04-03 17:49:30 +0300

    Add createCompleteElement utility function

---
+++ src/utils.js
@@ -0,0 +1,17 @@
+export function createCompleteElement (tag, classes = [], text = "", attributes = {}) {
+    const element =  document.createElement(tag);
+    
+    if(classes.length){
+        element.classList.add(...classes);
+    }
+
+    if(text){
+        element.textContent = text
+    }
+
+    for(const [key, value] of Object.entries(attributes)){
+        element.setAttribute(key, value);
+    }
+
+    return element;
+}
